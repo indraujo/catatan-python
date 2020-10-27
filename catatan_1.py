cac22c12ebd9485fd6ba2b010e7c192bbbe9d803
indraujo <indrakurniawan050793@gmail.com>
2020-10-27 14:09:23 +0700

    Create catatan_1.py

---
+++ catatan_1.py
@@ -0,0 +1,24 @@
+## Tipe Data
+# Integer
+print(type(1))
+
+# String
+print(type("1"))
+
+# Boolean
+print(type(True))
+
+# Float
+print(type(3.14))
+
+# Complex
+print(type(9j))
+
+# List
+print(type([1,2,3,4,5]))
+
+# Tuple
+print(type((1,2,3,4,5)))
+
+# Dictionary
+print(type({"a":1,"b":2,"c":3}))
