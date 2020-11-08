238f03a510de5399cb51a23a6930bcd957cc8fcd
indraujo <indrakurniawan050793@gmail.com>
2020-11-08 21:30:57 +0700

    update

--- Catatan Flask/catatan_3.py
+++ Catatan Flask/catatan_3.py
@@ -2,10 +2,10 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
+@app.route("/")
+def home():
     return render_template("index.html",content = "testing")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
